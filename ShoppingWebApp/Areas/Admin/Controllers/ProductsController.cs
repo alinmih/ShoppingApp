1fe35f448f87835eaf29ef59cb0da31e5ee05b0d
alin mihailescu <alin.d.mihailescu@gmail.com>
2020-01-03 12:09:21 +0200

    Added Product Edit

--- ShoppingWebApp/Areas/Admin/Controllers/ProductsController.cs
+++ ShoppingWebApp/Areas/Admin/Controllers/ProductsController.cs
@@ -40,6 +40,20 @@ namespace ShoppingWebApp.Areas.Admin.Controllers
             return View(await products.ToListAsync());
         }
 
+        // GET /admin/products/details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            //get the product from db
+            Product product = await context.Products.Include(x => x.Category).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            //retur page detail 
+            return View(product);
+        }
+
         // GET /admin/products/create
         public IActionResult Create()
         {
@@ -89,7 +103,6 @@ namespace ShoppingWebApp.Areas.Admin.Controllers
                 //set the property name
                 product.Image = imageName;
 
-
                 //add the page to db
                 context.Add(product);
                 await context.SaveChangesAsync();
@@ -103,19 +116,86 @@ namespace ShoppingWebApp.Areas.Admin.Controllers
         }
 
 
-        // GET /admin/products/details/5
-        public async Task<IActionResult> Details(int id)
+        // GET /admin/products/edit/5
+        public async Task<IActionResult> Edit(int id)
         {
-            //get the product from db
-            Product product = await context.Products.Include(x=>x.Category).FirstOrDefaultAsync(x=>x.Id == id);
+            Product product = await context.Products.FindAsync(id);
 
             if (product == null)
             {
                 return NotFound();
             }
-            //retur page detail 
+
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", product.CategoryId);
+
+            //retur product detail 
             return View(product);
         }
+
+        // POST /admin/products/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", product.CategoryId);
+
+            //check if model is valid
+            if (ModelState.IsValid)
+            {
+                //set the page prop
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                //get the slug which is equal to current
+                var slug = await context.Products.Where(x=>x.Id != id).FirstOrDefaultAsync(x => x.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product allready exists");
+                    return View(product);
+                }
+
+                //edit the image file
+                if (product.ImageUpload != null)
+                {
+                    //set the directory using webHostEnvironment
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+
+                    //remove the old image if exists one
+                    if (!string.Equals(product.Image, "noimage.png"))
+                    {
+                        string oldImagePath = Path.Combine(uploadsDir, product.Image);
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    //set the name of the image to be unique
+                    string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    //set the full image path
+                    string filePath = Path.Combine(uploadsDir, imageName);
+
+                    //upload using FileStream class
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                    //set the property name
+                    product.Image = imageName;
+                }
+
+
+                //add the page to db
+                context.Update(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been edited!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+
 
 
     }
